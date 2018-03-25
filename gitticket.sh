89b950294c0674cce5cf5d7117ef1ac37282cd79
ykostr <ykostr@softserveinc.com>
2018-03-25 21:55:51 +0300

    fixed ticket number retrieving

--- gitticket.sh
+++ gitticket.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ticket_number="$(git rev-parse --abbrev-ref HEAD | grep -E -i -o [0-9]{5})"
-message="'$*'"
-if [ -z "$ticket_number" ]; then
-	git commit -m $message
+message="$*"
+if [ -z $ticket_number ]; then
+	git commit -m "$message"
 else
 	git commit -m "[RMM-"$ticket_number"] "$message
 fi
