227aa0a223e710fe580ac2c95aa39b0d6eefa3e3
ykostr <ykostr@softserveinc.com>
2018-03-25 21:39:27 +0300

    asdf

--- gitticket.sh
+++ gitticket.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-ticket_number=$(git branch | grep -E -i -o [0-9]{5})
-git commit -m "[RMM-"$ticket_number"] "$1
+ticket_number=$(git rev-parse --abbrev-ref HEAD | grep -E -i -o [0-9]{5})
+if [ -z $ticket_number ]; then
+	git commit -m $1
+else
+	git commit -m "[RMM-"$ticket_number"] "$1
+fi
--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 mkdir -p ~/.local/bin
 cp ./gitticket.sh ~/.local/bin
 chmod +x ~/.local/bin/gitticket.sh
-echo "alias gcommit=\"gitticket.sh\"" >> ~/.profile
-. ~/.profile
+echo "alias gcommit=\"gitticket.sh\"" >> ~/.bash_profile
+. ~/.bash_profile
